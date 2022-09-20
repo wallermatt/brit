e14189ee37125a7d4f3d71f05da8346bdeb6db5d
wallermatt <waller.matthew.alt@gmail.com>
2022-09-20 14:33:08 +0100

    login

--- brit/prices/urls.py
+++ brit/prices/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path('items', views.items, name='items'),
     path('summary', views.summary, name='summary'),
     path('new_item', views.new_item, name='new_item'),
+    path('signup', views.signup, name='signup'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- brit/prices/views.py
+++ brit/prices/views.py
@@ -1,20 +1,25 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.db.models import Sum
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
 from .models import Item
 
-# Create your views here.
+@login_required(login_url='accounts/login/')
 def items(request):
     items = Item.objects.all().order_by('name')
     return render(request, 'items.html', {'items': items})
 
 
+@login_required(login_url='accounts/login/')
 def summary(request):
     total_cost = Item.objects.aggregate(Sum('price'))['price__sum']
     return render(request, 'summary.html', {'total_cost': total_cost})
 
 
+@login_required(login_url='accounts/login/')
 def new_item(request):
     if request.method == 'POST':
         item_name = request.POST['name']
@@ -28,3 +33,18 @@ def new_item(request):
         new_item.save()
         return HttpResponseRedirect('items')
     return render(request, 'new_item.html') 
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return HttpResponseRedirect('items')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
